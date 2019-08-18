86dcc0cfdff6e1e0b6e97dda78e34643868bd4fc
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-08-18 10:01:12 -0400

    navbar created

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import logo from '../assets/gold_logo.png';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <img src={logo} alt='logo' />
+      <div className='navbar-text'>Merchandise Order Form</div>
+    </div>
   );
 }
 
